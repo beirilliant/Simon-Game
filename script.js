dc1686cb073ffd0ccfb8087fc0c66d71d727dd3c
Bei Wang <bei.wang@beirilliant.com>
2017-10-27 11:38:58 +1000

    Make the switchBtn toggleable

---
+++ script.js
@@ -0,0 +1,12 @@
+var isOn = 0;
+$("#switchBtn").click(function() {
+    if (isOn === 0) {
+        isOn = 1;
+        $("#switchBtn").css("left", "42px");
+        gameReady();
+    } else if (isOn === 1) {
+        isOn = 0;
+        $("#switchBtn").css("left", "4px");
+        endGame();
+    }
+});
