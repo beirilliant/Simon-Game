4b2d37c5b25f10da839cf86eec7edd2b388ab980
Bei Wang <bei.wang@beirilliant.com>
2017-11-01 13:58:09 +1000

    Include audio files and test audio play

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var isOn = false;
 var count = 0;
+var sound = {
+    green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+    red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+    yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+    blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")
+};
 $("#switchBtn").click(function() {
     if (isOn === false) {
         isOn = true;
@@ -23,6 +29,7 @@ function endGame() {
 
 function startGame() {
     alert("game starts!");
+    $(sound.green).trigger('play');
 }
 
 $("#start").click(function() {
